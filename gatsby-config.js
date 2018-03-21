2f378cec5577563d7f12afc62af8d78611019860
Darin Im <darinim@gmail.com>
2018-03-20 21:45:33 -0700

    remove filler

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'blog',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -23,7 +23,7 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          Gatsby
+          blog
         </Link>
       </h1>
     </div>
